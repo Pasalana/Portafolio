fa96da8c11f95e1629e47b13228003f90121cddf
vale correa <Pasalana@MacBook-Pro-de-Vale.local>
2020-12-24 21:41:14 -0300

    portafolio2

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+  // smoth scroll..
+    $( "a" ).click( function (event) {
+       if  ( this .hash !==  "" ) {
+       event.preventDefault();
+       var  gato =  this .hash;
+      $( 'html, body' ).animate({
+        scrollTop: $(gato).offset().top
+      },  900 ,  function (){
+       window .location.hash = gato;
+     });
+      }  // Fin del if
+    });
+
+
+});
